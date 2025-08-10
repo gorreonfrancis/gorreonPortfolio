0222f66963686d2f026b5bb71fb1e3b88d7fd0a0
markfrancis <markfrancispgorreon@iskolarngbayan.pup.edu.ph>
2025-08-10 20:47:27 +0800

    Use fetch for file sectioning

--- javascripts/nav_bar.js
+++ javascripts/nav_bar.js
@@ -1,3 +1,7 @@
+ fetch("nav_bar.html")
+      .then(res => res.text())
+      .then(data => document.getElementById("nav_bar").innerHTML = data);
+ 
  function toggleSidebar() {
         const sidebar = document.querySelector("#sidebar");
         sidebar.classList.toggle("invisible");
