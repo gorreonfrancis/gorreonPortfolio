9cf957ebf6c322b7a86cd38ba8ca640f90bb5e67
markfrancis <markfrancispgorreon@iskolarngbayan.pup.edu.ph>
2025-08-13 09:08:43 +0800

    Performed sectioning for code readability

--- javascripts/layout.js
+++ javascripts/layout.js
@@ -1,8 +1,19 @@
  fetch("nav_bar.html")
       .then(res => res.text())
       .then(data => document.getElementById("nav_bar").innerHTML = data);
- 
+
+fetch("home.html")
+      .then(res => res.text())
+      .then(data => document.getElementById("home").innerHTML = data);
+
+ fetch("about.html")
+      .then(res => res.text())
+      .then(data => document.getElementById("about").innerHTML = data);
+
  function toggleSidebar() {
         const sidebar = document.querySelector("#sidebar");
         sidebar.classList.toggle("invisible");
       }
+
+
+
